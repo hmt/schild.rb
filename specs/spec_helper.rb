57745c0db3eebc06bfbd46b62c47853562d61765
hmt <dev@hmt.im>
2015-11-09 20:58:04 +0100

    ready for jruby 9000

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,9 +1,12 @@
-# encoding: UTF-8
 require 'sequel'
 require 'minitest/autorun'
 require 'minitest/rg'
 require 'envyable'
+
 Envyable.load('./config/env.yml', 'local_test')
+if RUBY_ENGINE == 'jruby'
+  ENV['S_ADAPTER'] = "jdbc:mysql"
+end
 
 require 'schild'
 include SchildErweitert
